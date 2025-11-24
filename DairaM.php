1d9717304da4f2916d6ff9a5e35b75752b51424c
martinezdaira <martinezmontesdaira@gmail.com>
2025-11-24 14:12:01 -0400

    JS

---
+++ DairaM.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>QUÉ ES JAVA?</h1>
+<p>Sé que Java es un lenguaje de programación que se usa para que las paginas web sean interactivas</p>
+<h1>SINTAXIS DE JAVASCRIPT</h1>
+<p>Se usan variables para guardar los datos, if para mostrar decisiones y for para repetir las instrucciones. </p>
+<h1>ESTRUCTURS REPETITIVAS</h1>
+<p>En JavasScript las estructuras repetitivos permiten repetir un código varias , las más usadas son:for,while, y do-while</p>
+<h1>POO (atributos y métodos)</h1>
+<p>POO, significa <strong>programación orientada a objetos</strong> que organiza el código en objetos, los cuales combinan datos (atributos o propiedades) y comportamientos (métodos o funciones)</p>
+<h2>ATRIBUTOS</h2>
+<p>son las características que hay dentro, por ejemplo: 
+    -Uniforme
+    -Comportamiento
+    -Horarios
+</p>
+<h2>MÉTODOS</h2>
+<p>son las acciones que puede realizar un objeto, por ejemplo:
+    -Estudiar
+    -Jugar
+    -Ayudar en casa
+    -Dar cariño
+    </p>
+<h1>HERENCIA </h1>
+<p>La herencia permite a las subclases heredar atributos y métodos de la superclase, lo que significa que la subclase puede acceder y utilizar características y comportamientos definidos de la superclase.</p>
+
+</body>
+</html>
