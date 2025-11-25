863a42df46be606fd3c09d7947a516379568c9f5
martinezdaira <martinezmontesdaira@gmail.com>
2025-11-25 11:56:33 -0400

    inf CRUD

--- CRUD.php
+++ CRUD.php
@@ -7,7 +7,17 @@
 </head>
 <body>
     
-<h1>Que signifcan las iniciales de CRUD?</h1>
-<p>La <strong>C</strong> es de CREATE (crear), la <strong>R</strong> es de READ (leer),la <strong>U</strong> es de UPDATE (actualizar) y la <strong>D</strong> es DELETE (eliminar) </p>
+<h1>Qué signifcan las iniciales de CRUD?</h1>
+<p>La <strong>C</strong> es de CREATE (crear), la <strong>R</strong> es de READ (leer),la <strong>U</strong> es de UPDATE (actualizar) y la <strong>D</strong> es DELETE (eliminar). </p>
+<br>
+<h1>Para qué sirve CRUD?</h1>
+<p>CRUD es una forma básica de manejar datos en un sistema. Se usa en casi cualquier aplicación que guarda información, como redes sociales o tiendas online.</p>
+ <h1>Qué diferencias hay entre cada una de las operaciones del CRUD?</h1>
+ <p>-Create agrega datos nuevos.
+    -Read obtiene datos existentes.
+    -Update modifica datos existentes.
+    -Delete elimina datos existentes.</p>
+ <h1>Por qué es importante un CRUD en una aplicación?</h1>
+    <p>Es importante porque permite gestionar la información básica de una aplicación: crear, ver, actualizar y eliminar datos,sin estas operaciones la aplicación no podría funcionar correctamente ni permitir que los usuarios controlen su información.</p>
 </body>
 </html>
