a157207eecb9cfda3fbb26bfdb09ad6e1b186d31
AritzPS <ale.noe.1405@gmail.com>
2025-11-25 12:04:20 -0400

    Javacito

---
+++ AritzPS.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sintaxis  Java</h1>
+    <p>import java.util.Scanner;
+        public class nombre {
+            public static void main(String[] args) {
+            }
+        }
+        Es la estructura básica de un programa en Java.
+    <h1>Estructuras repetitivas</h1>
+    <p>Las estructuras repetitivas en Java son bucles que permiten ejecutar un bloque de código varias veces. Las más comunes son:
+        - for: se utiliza cuando se conoce el número exacto de iteraciones.
+        - while: se utiliza cuando no se conoce el número exacto de iteraciones y depende de una condición.
+        - do-while: similar a while, pero garantiza que el bloque de código se ejecute al menos una vez.
+    <h1>POO</h1>
+    <p>La Programación Orientada a Objetos (POO) es un paradigma de programación que utiliza "objetos" para representar datos y métodos. Los conceptos clave de la POO incluyen:
+        - Clases: plantillas para crear objetos.
+        - Objetos: instancias de clases que contienen atributos y métodos.
+        - Herencia: permite que una clase herede atributos y métodos de otra clase.
+    <h1>Herencia</h1>
+    <p>La herencia es un concepto fundamental en la Programación Orientada a Objetos (POO) que permite a una clase (subclase) heredar atributos y métodos de otra clase (superclase). Esto promueve la reutilización del código y establece una relación jerárquica entre las clases. Por ejemplo, si tenemos una clase "Animal" con atributos y métodos comunes, podemos crear subclases como "Perro" y "Gato" que hereden esas características y agreguen sus propias particularidades.
+</body>
+</html>
