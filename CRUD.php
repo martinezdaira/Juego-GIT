e7183a0bbd19b3fdb441b7bc777b1f0f17629db2
martinezdaira <martinezmontesdaira@gmail.com>
2025-11-25 11:30:43 -0400

    iniciales del CRUD

---
+++ CRUD.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+    
+<h1>Que signifcan las iniciales de CRUD?</h1>
+<p>La <strong>C</strong> es de CREATE (crear), la <strong>R</strong> es de READ (leer),la <strong>U</strong> es de UPDATE (actualizar) y la <strong>D</strong> es DELETE (eliminar) </p>
+</body>
+</html>
