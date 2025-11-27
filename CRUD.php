63ad3dd55f304273dd629f3014375c75b69d1704
AritzPS <ale.noe.1405@gmail.com>
2025-11-27 12:42:55 -0400

    Crudcito

--- CRUD.php
+++ CRUD.php
@@ -18,5 +18,24 @@
     -Delete elimina datos existentes.</p>
  <h1>Por qué es importante un CRUD en una aplicación?</h1>
     <p>Es importante porque permite gestionar la información básica de una aplicación: crear, ver, actualizar y eliminar datos,sin estas operaciones la aplicación no podría funcionar correctamente ni permitir que los usuarios controlen su información.</p>
+<br>
+<h1>CRUD-ALE</h1>
+<h2>INICIALES DE CRUD.-</h2>
+<p>CRUD es el acrónimo de las cuatro operaciones básicas para interactuar con datos: Crear, Read (Leer), Update (Actualizar) y Delete (Eliminar). Es un concepto fundamental en programación y se refiere a las funciones esenciales de la persistencia de datos, es decir, cómo los datos son añadidos,
+    consultados, modificados y borrados en un sistema. </p>
+<h2>UTILIDAD DE CRUD .-</h2>
+<p>se utilizan en aplicaciones de almacenamiento persistente, lo que significa que estas aplicaciones conservarán sus datos incluso después de que el sistema se apague. Estas son diferentes de las operaciones con datos almacenados en almacenamiento volátil, como la memoria de acceso aleatorio o 
+    los archivos de caché.</p>
+<h2>DIFERENCIA ENTRE SUS OPERACIONES.-</h2>
+<h3>CREAR</h3>
+<p>La operación CREATE añade un nuevo registro a una base de datos. </p>
+*<h3>LEER</h3>
+<p>La operación READ recupera datos de una base de datos sin modificarla. </p>
+<h3>ACTUALIZAR</h3>
+<p>La operación UPDATE modifica los datos existentes en una base de datos. </p>
+<h3>ELIMINAR</h3>
+<p>La operación DELETE elimina datos existentes de una base de datos. </p>
+<h2>IMPORTANCIA DE CRUD.-</h2>
+<p>CRUD es fundamental para el desarrollo de aplicaciones que manejan datos, ya que proporciona un marco estructurado para interactuar con la información almacenada. Permite a los desarrolladores crear interfaces de usuario intuitivas y eficientes, facilitando la gestión de datos por parte de los usuarios finales. Además, CRUD es esencial para mantener la integridad y coherencia de los datos en sistemas complejos.</p>
 </body>
 </html>
