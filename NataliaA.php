81245677106a9368703c4326fc88dc6ddded9a9c
EmanuelZenteno <zentenogutierrezemanuel@pedropoveda.edu.bo>
2025-11-27 12:49:11 -0400

    trabajito

---
+++ NataliaA.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Java</h1>
+    <p>Java es un lenguaje de programacion y plataforma informatica orientada a objetos. es uno de los lenguajes mas usados en el mundo por su versatilidad.seguridad y portabilidad</p>
+    <h1>sintaxis java</h1>
+    <p>java tiene un conjunto de reglas que define como se escriben los programas en este lenguaje, se utiliza (main) para definir el punto de entrada de la aplicacion, las variables se declaran especificando su tipo de nombre y dato, las estructuras de controlcomo(if,for,while)permiten controlar el flujo de ejecucion del programa</p>
+    <h1>estructurad repetitivas</h1>
+    <p> las estructuras repetitivas en java permiten ejecutar un bloque de codigo de manera repetitiva hasta que se cumpla una condicion determinada,las mas comunes son:for,while y do-while</p>
+    <h1>POO (atriburtos y metodos)</h1>
+    <p> la programacion orientada a objetos (POO) en java se basa en el concepto de objetos que contiene atributos y metodos. los atributos representan las caracteristicas del objeto, mientras que los metodos define su comportamiento. La POO permite la encapsulacion, herencia y polimorfismo, facilitando la organizacion y reutilizacion del codigo</p>
+    <h1>herencia</h1>
+    <p> la herencia en java es un mecanismo que permite a una clase "subclase" heredar atributos y metodos de otra clase "superclase". esto permite la reutilizacion del codigo y la creacion de jerarquias de clases.la subclase puede agregar sus propios atributos y metodos ademas de heredarlos a la super clase</p>
+    
+    
+
+</body>
+</html>
