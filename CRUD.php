3603ed90f4322c4ee5459cd07e098fa8ab383a9c
martinezdaira <martinezmontesdaira@gmail.com>
2025-11-27 12:42:48 -0400

    crud

--- CRUD.php
+++ CRUD.php
@@ -6,7 +6,7 @@
     <title>CRUD</title>
 </head>
 <body>
-    
+    <h1>CRUD-Daira</h1>
 <h1>Qué signifcan las iniciales de CRUD?</h1>
 <p>La <strong>C</strong> es de CREATE (crear), la <strong>R</strong> es de READ (leer),la <strong>U</strong> es de UPDATE (actualizar) y la <strong>D</strong> es DELETE (eliminar). </p>
 <h1>Para qué sirve CRUD?</h1>
